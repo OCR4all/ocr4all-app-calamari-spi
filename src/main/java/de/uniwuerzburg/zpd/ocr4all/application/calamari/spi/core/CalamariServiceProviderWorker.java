f0a41faefd2978b301ff089c029f80c0813b1fb0
Herbert Baier <baier@itbaier.de>
2024-07-19 12:59:11 +0200

    msa/event message content

--- src/main/java/de/uniwuerzburg/zpd/ocr4all/application/calamari/spi/core/CalamariServiceProviderWorker.java
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/calamari/spi/core/CalamariServiceProviderWorker.java
@@ -959,7 +959,7 @@ public abstract class CalamariServiceProviderWorker<C extends ProcessorCore.Call
 					@Override
 					protected void handle(EventSPI event) {
 						final String message = "received event " + event.getType().name() + " (" + event.getCreatedAt()
-								+ ") - " + event.getMessage();
+								+ ") - " + event.getMessage().getContent();
 
 						logger.debug(getProcessorIdentifier() + ": " + message);
 
